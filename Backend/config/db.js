e0b917e57eccd20e45ebf84bdab595fda82431b4
Haroon0520 <abukababy@gmail.com>
2025-12-16 12:54:41 +0530

    updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:186312@cluster0.ovanjzw.mongodb.net/a-m-autostores').then(()=>console.log("shinzou sasageyo"));
+    await mongoose.connect('').then(()=>console.log("shinzou sasageyo"));
 }
